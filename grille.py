a89406d0bd83b071e13769f74bae2d40c650e67d
BuildTools <unconfigured@null.spigotmc.org>
2023-09-16 21:33:07 +0200

    Début Affichage de la grille

--- grille.py
+++ grille.py
@@ -1,11 +1,10 @@
-'''print('[-][-][-]\n[-][-][-]\n[-][-][-]')'''
-
 class Grille:
     def __init__( self , taillex , tailley ):
         self.taillex = taillex
         self.tailley = tailley
         self.grille = []
         self.creer_grille()
+        self.affichage_grille()
 
     
     def creer_grille(self):
@@ -15,6 +14,9 @@ class Grille:
             for j in range(self.tailley):                         
                 ligne.append(icone)
             self.grille.append(ligne)
+
+    def affichage_grille(self):
+        pass
 
 
 g = Grille(3,3)
