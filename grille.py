61a906048d169df5db8f8fc7a1116f3cbbfb1d44
ionbenbigs <ionbenbig@gmail.com>
2023-09-17 22:16:08 +0200

    grille fini avec l'affichage et la creation de grille

--- grille.py
+++ grille.py
@@ -6,16 +6,29 @@ class Grille:
         self.tailley = tailley
         self.grille = []
         self.creer_grille()
+        self.afficher_grille()
 
     
     def creer_grille(self):
         icone = 'X'
-        for i in range (self.taillex):
+        for i in range(self.taillex):
             ligne=[]
             for j in range(self.tailley):                         
                 ligne.append(icone)
             self.grille.append(ligne)
+    
 
-
+    def afficher_grille(self):
+        icone = 1
+        
+        for p in range(self.taillex):
+            
+            for h in range(self.tailley):
+                print(f"[{icone}]",end= "")
+                icone+=1
+            print('')    
 g = Grille(3,3)
-print(g.grille)
+
+# [X] [X] [X]
+# [X] [X] [X]
+# [X] [X] [X]
